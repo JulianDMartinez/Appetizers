13af993f6a8f8cf0a7792a0126e4ee2aed3c0bb0
Julian <julian.martinez.s@outlook.com>
2020-12-24 17:17:17 -0600

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Julian Martinez on 12/24/20.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
